18d62823b2b8649b92937e3b99fdf871f9f80cef
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-11-23 10:53:50 -0700

    Adding addProperty

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -183,6 +183,50 @@ const getAllProperties = function (options, limit = 10) {
     .catch((err) => console.error('Error executing query', err.stack));
 };
 
+const addProperty = function (property) {
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  const queryString = `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, queryParams)
+    .then((res) => res.rows[0])
+    .catch((err) => console.error('Error executing query', err.stack));
+};
+git 
 
 module.exports = {
   getUserWithEmail,
